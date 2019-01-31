ecee39ed609ff4bd3114039f195f8b7eff0a88d2
Rich Taylor <github@rsty.org>
2019-01-31 11:26:44 +0000

    UPDATE: hey now it does some actually useful stuff

--- proxy.go
+++ proxy.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	//"math/rand"
 	"io/ioutil"
+	//	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -37,14 +39,19 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 	path := fmt.Sprintf("%s", ctx.Path())
 	_, in_map := url_map[host][path]
 
+	// TODO: dunno what fastly calls this header, so im just gonna make it up
+	ingress_protocol := ctx.Request.Header.Peek("X-Forwarded-Proto")
+	if ingress_protocol == nil {
+		// assume http? TODO
+		ingress_protocol = []byte("http")
+	}
+
 	if string(ctx.Method()) == "GET" && in_map {
 		// we only ever redirect GETs
 		// TODO: we should really think hard about the above assumption
-		fmt.Println("Redirect...")
+		fmt.Println("Mode: Redirect")
 
-		// TODO: how do we derive the protocol?
-		protocol := "https"
-		redirect_location := fmt.Sprintf("%s://%s%s", protocol, host, path)
+		redirect_location := fmt.Sprintf("%s://%s%s", ingress_protocol, host, path)
 
 		ctx.SetStatusCode(fasthttp.StatusMovedPermanently)
 		ctx.Response.Header.Set("Location", redirect_location)
@@ -52,38 +59,53 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 
 	} else {
 
-		fmt.Println("Proxy...")
+		fmt.Println("Mode: Proxy")
 
 		// response should indicate we proxied it
 		ctx.Response.Header.Set("X-Redirector", "proxy")
 
 		// start building a request
 		client := &http.Client{
-			// do not follow redirects.
+			// do not follow redirects; if there's a redirect issued upstream
+			// then we should spit that request out as-is. i think.
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-        		return http.ErrUseLastResponse
-    		},
+				return http.ErrUseLastResponse
+			},
 		}
 
 		// rebuild URL to request
-		fq_url := fmt.Sprintf("%s://%s%s", "http", host, ctx.Path())
+		proxy_url := fmt.Sprintf("%s://%s%s", ingress_protocol, host, ctx.Path())
 
-		// TODO: chunked responses?
-		req, _ := http.NewRequest("GET", fq_url, nil)
+		fmt.Println("Protocol:", string(ingress_protocol))
+		fmt.Println("URL:", proxy_url)
+		fmt.Println("Method:", string(ctx.Method()))
+
+		// copy any body data
+		ingress_body := ctx.PostBody()
+		if len(ingress_body) == 0 {
+			ingress_body = nil
+			fmt.Println("Body: nil")
+		} else {
+			fmt.Println("Body:", string(ingress_body))
+		}
+
+		// TODO: how do we handle chunked responses
+		proxy_req, _ := http.NewRequest(string(ctx.Method()), proxy_url, bytes.NewBuffer(ingress_body))
 
 		// copy headers from ingress request into proxy request
-		ctx.Request.Header.VisitAll(func (key, value []byte) {
-			req.Header.Set(string(key), string(value))
+		ctx.Request.Header.VisitAll(func(key, value []byte) {
+			proxy_req.Header.Set(string(key), string(value))
+			fmt.Println("Header:", string(key), ":", string(value))
 		})
 
-		resp, err := client.Do(req)
+		proxy_resp, err := client.Do(proxy_req)
 		if err != nil {
 			// todo?
 			panic(err)
 		}
-		defer resp.Body.Close()
+		defer proxy_resp.Body.Close()
 
-		b, err := ioutil.ReadAll(resp.Body)
+		b, err := ioutil.ReadAll(proxy_resp.Body)
 
 		if err != nil {
 			log.Fatal(err)
@@ -91,16 +113,18 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 			panic(err)
 		}
 
-		for k, v := range resp.Header {
-              ctx.Response.Header.Set(string(k), string(v[0]))
-      	}
-
-      	ctx.SetStatusCode(resp.StatusCode)
-
 		body := string(b)
 
-		fmt.Fprintf(ctx, "body")
-		fmt.Println(len(body))
+		// copy response headers back to egress response
+		for k, v := range proxy_resp.Header {
+			ctx.Response.Header.Set(string(k), string(v[0]))
+		}
+
+		// copy status code back to egress response
+		ctx.SetStatusCode(proxy_resp.StatusCode)
+
+		// copy proxy response back to egress response
+		fmt.Fprintf(ctx, body)
 	}
 }
 
